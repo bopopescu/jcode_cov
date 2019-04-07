8b1e098852606c5e06180bf6f2607a5b3d9b9f25
zhaobin11 <zhaobin11@meituan.com>
2019-04-07 21:24:41 +0800

    remote dir

--- CovLogger.py
+++ CovLogger.py
@@ -13,7 +13,6 @@ PROJECT_NAME = os.path.basename(PROJECT_ROOT)
 
 LOG_DIR = os.path.join(PROJECT_ROOT, "out")
 default_log_file = os.path.join(LOG_DIR, "{}.log".format(PROJECT_NAME))
-default_log_file = os.path.join(LOG_DIR, )
 
 
 def h1(self, title):
