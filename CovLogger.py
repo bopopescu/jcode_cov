efe1a583250a6c738fdfcb6dec35d34c41f1862a
zhaobin11 <zhaobin11@meituan.com>
2019-04-07 21:22:35 +0800

    update rotating size

--- CovLogger.py
+++ CovLogger.py
@@ -9,38 +9,41 @@ import logging.handlers
 from qcs_env_coverage.venv import colorlog
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-LOG_DIR = os.path.join(PROJECT_ROOT, 'out')
-default_log_file = os.path.join(LOG_DIR, 'qcs_env_coverage.log')
+PROJECT_NAME = os.path.basename(PROJECT_ROOT)
+
+LOG_DIR = os.path.join(PROJECT_ROOT, "out")
+default_log_file = os.path.join(LOG_DIR, "{}.log".format(PROJECT_NAME))
+default_log_file = os.path.join(LOG_DIR, )
 
 
 def h1(self, title):
-    self.info('=' * 120)
+    self.info("=" * 120)
     self.info(title.upper())
-    self.info('=' * 120)
+    self.info("=" * 120)
 
 
 def h2(self, title):
-    self.info('=' * 100)
+    self.info("=" * 100)
     self.info(title.upper())
-    self.info('=' * 100)
+    self.info("=" * 100)
 
 
 def h3(self, title):
-    self.info('=' * 80)
+    self.info("=" * 80)
     self.info(title.upper())
-    self.info('=' * 80)
+    self.info("=" * 80)
 
 
 def sep1(self):
-    self.info('-' * 80)
+    self.info("-" * 80)
 
 
 def sep2(self):
-    self.info('-' * 60)
+    self.info("-" * 60)
 
 
 def sep3(self):
-    self.info('-' * 40)
+    self.info("-" * 40)
 
 
 def _decorate_logger(logger):
@@ -68,20 +71,20 @@ class CoverageLog(object):
         # Get root logger if name is None
         mylogger = colorlog.getLogger(name)
         mylogger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('[%(asctime)s][%(levelname)s] - %(message)s')
+        formatter = logging.Formatter("[%(asctime)s][%(levelname)s] - %(message)s")
         color_formatter = colorlog.ColoredFormatter(
             "%(log_color)s[%(asctime)s][%(levelname)s] - %(message)s",
             datefmt=None,
             reset=True,
             log_colors={
-                'DEBUG': 'cyan',
-                'INFO': 'blue',
-                'WARNING': 'purple',
-                'ERROR': 'red',
-                'CRITICAL': 'bold_red',
+                "DEBUG": "cyan",
+                "INFO": "blue",
+                "WARNING": "purple",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
             },
             secondary_log_colors={},
-            style='%'
+            style="%"
         )
 
         s_handler = colorlog.StreamHandler()
@@ -106,5 +109,5 @@ class CoverageLog(object):
 # Setup default logger
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
-logger = CoverageLog.get_logger(name='DefaultCoverage', log_file=default_log_file)
+logger = CoverageLog.get_logger(name="DefaultCoverage", log_file=default_log_file)
 _decorate_logger(logger)
